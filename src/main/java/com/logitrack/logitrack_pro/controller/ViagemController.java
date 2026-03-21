9d8af826ba555e042a6a6a4c5c553bf92d83ecde
robertogouveia <robertobgouveia@outlook.com>
2026-03-21 19:17:05 -0300

    altera controllers

--- src/main/java/com/logitrack/logitrack_pro/controller/ViagemController.java
+++ src/main/java/com/logitrack/logitrack_pro/controller/ViagemController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/viagens")
 public class ViagemController {
